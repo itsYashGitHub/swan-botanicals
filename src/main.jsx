32bbf76883f52fbd3d0426686dbdedfd129a031a
Yashovardhan <singhyashovardhan9@gmail.com>
2025-08-14 21:57:23 +0530

    added vercel.json 3

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
+    <CartProvider>
+      <App />
+    </CartProvider>
   </StrictMode>
 );
